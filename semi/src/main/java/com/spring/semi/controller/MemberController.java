64d7953f89fdfed54e103f06b0a19aa7861170c1
sooyeon8888 <pansy0828sy@gmail.com>
2025-10-20 16:39:20 +0900

    변수작성 방법 수정

--- semi/src/main/java/com/spring/semi/controller/MemberController.java
+++ semi/src/main/java/com/spring/semi/controller/MemberController.java
@@ -51,8 +51,8 @@ public class MemberController {
 		if(findDto == null) return "redirct:login?error";
 		if(findDto.getMemberPw().equals(memberDto.getMemberPw()) == false) return "redirct:login?error";
 		
-		session.setAttribute("login_id", findDto.getMemberId());
-		session.setAttribute("login_level", findDto.getMemberLevel());
+		session.setAttribute("loginId", findDto.getMemberId());
+		session.setAttribute("loginLevel", findDto.getMemberLevel());
 		
 		return "/WEB-INF/views/home.jsp";
 	}
@@ -61,8 +61,8 @@ public class MemberController {
 	public String logout(
 			HttpSession session
 			) {
-		session.removeAttribute("login_id");
-		session.removeAttribute("login_level");
+		session.removeAttribute("loginId");
+		session.removeAttribute("loginLevel");
 		
 		return "redirect:/";
 	}
