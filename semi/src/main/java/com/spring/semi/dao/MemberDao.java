eb9971c6b91f52c95b93ed8eea0480376d3e6cc2
SadMental <sadrapper99@gmail.com>
2025-10-20 15:41:06 +0900

    MemberDao 및 Controller 작업

---
+++ semi/src/main/java/com/spring/semi/dao/MemberDao.java
@@ -0,0 +1,62 @@
+package com.spring.semi.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.spring.semi.dto.MemberDto;
+import com.spring.semi.mapper.MemberMapper;
+
+@Repository
+public class MemberDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	public void insert(MemberDto memberDto) {
+		String sql = "insert into "
+				+ "member(member_id, member_pw, member_nickname, member_email, "
+								+ "member_description, member_auth, member_animal) "
+				+ "values(?, ?, ?, ?, ?, ?, ?)";
+		
+		Object[] params = {
+				memberDto.getMemberId(), memberDto.getMemberPw(),
+				memberDto.getMemberNickname(), memberDto.getMemberEmail(),
+				memberDto.getMemberDesciption(),
+				memberDto.getMemberAuth(), memberDto.getMemberAnimal()};
+		
+		jdbcTemplate.update(sql, params);
+	}
+	
+	public boolean delete(String member_id) {
+		String sql = "delete from member where member_id = ?";
+		Object[] params = {member_id};
+		return jdbcTemplate.update(sql, params) > 0;
+	}
+	
+	public boolean updateForUser(MemberDto memberDto) {
+		String sql = "update set member_nickname = ?, member_pw = ?, member_email = ?, "
+												+ "member_description = ?, member_auth = ?, member_animal = ?"
+												+ "where member_id = ?";
+		Object[] params = {
+				memberDto.getMemberPw(),
+				memberDto.getMemberNickname(), memberDto.getMemberEmail(),
+				memberDto.getMemberDesciption(),
+				memberDto.getMemberAuth(), memberDto.getMemberAnimal(),
+				memberDto.getMemberId()};
+		
+		return jdbcTemplate.update(sql, params) > 0;
+	}
+	
+	public MemberDto selectOne(String member_id) {
+		String sql = "select * from member where member_id = ?";
+		Object[] params = {member_id};
+		List<MemberDto> list = jdbcTemplate.query(sql, memberMapper, params);
+		return list.isEmpty()? null : list.get(0);
+	}
+	
+	
+}
