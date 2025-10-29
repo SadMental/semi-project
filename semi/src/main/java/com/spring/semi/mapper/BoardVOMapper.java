b560479c2eb21b999a90c53387dda05e38afd225
SadMental <sadrapper99@gmail.com>
2025-10-29 16:11:32 +0900

    header 수정. 오류많음 조심

---
+++ semi/src/main/java/com/spring/semi/mapper/BoardVOMapper.java
@@ -0,0 +1,29 @@
+package com.spring.semi.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.spring.semi.vo.BoardVO;
+
+@Component
+public class BoardVOMapper  implements RowMapper<BoardVO> {
+
+	@Override
+	public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return BoardVO.builder()
+				.boardCategoryNo(rs.getInt("board_category_no"))
+				.boardNo(rs.getInt("board_no"))
+				.boardTitle(rs.getString("board_title"))
+				.boardWriter(rs.getString("board_writer"))
+				.boardWtime(rs.getTimestamp("board_wtime"))
+				.boardEtime(rs.getTimestamp("board_etime"))
+				.boardLike(rs.getInt("board_like"))
+				.boardView(rs.getInt("board_view"))
+				.boardReply(rs.getInt("board_reply"))
+				.headerName(rs.getString("header_name"))
+				.build();
+	}
+}
