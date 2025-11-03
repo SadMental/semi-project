e295c9cdb0544f92bb60beddb6c50f83384d3fb1
SadMental <sadrapper99@gmail.com>
2025-11-03 10:29:28 +0900

    로그인

--- semi/src/main/java/com/spring/semi/aop/MemberLoginInterceptor.java
+++ semi/src/main/java/com/spring/semi/aop/MemberLoginInterceptor.java
@@ -1,5 +1,28 @@
 package com.spring.semi.aop;
 
-public class MemberLoginInterceptor {
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.spring.semi.error.UnauthorizationException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class MemberLoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String loginId = (String) session.getAttribute("loginId");
+		if(loginId == null) {
+			throw new UnauthorizationException("로그인하셔야합니다.");
+		}
+		//
+		return true;
+	}
+	
 }
