c6bc5b0adea554fe7d28ed0b789fa68852190eae
SadMental <sadrapper99@gmail.com>
2025-11-03 10:28:45 +0900

    어드민

--- semi/src/main/java/com/spring/semi/aop/AdminInterceptor.java
+++ semi/src/main/java/com/spring/semi/aop/AdminInterceptor.java
@@ -6,17 +6,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.spring.semi.error.NeedPermissionException;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Service
 public class AdminInterceptor implements HandlerInterceptor{
-   public boolean preHandle(HttpServletRequest request,HttpServletRequest response,
-		   Object Handler) throws Exception{
-	   HttpSession session =request.getSession();
-	   Integer loginLevel = (Integer) session.getAttribute("loginLevel");
-	   int limit = 2; //   
-	   if(loginLevel<limit) //  레벨  2이상 관리자권한 
-		   throw new NeedPermissionException("권한 부족");
-	   return true;
-   }
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,HttpServletResponse response,
+			Object Handler) throws Exception{
+		HttpSession session =request.getSession();
+		Integer loginLevel = (Integer) session.getAttribute("loginLevel");
+		int limit = 0; //   
+		if(loginLevel == limit) //  레벨 0 관리자권한 
+			throw new NeedPermissionException("권한 부족");
+		return true;
+	}
 }
